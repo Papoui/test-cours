4cedef8bddbd8572635e5d731c55953dacd6af28
Papoui <darckrim64@gmail.com>
2019-08-27 04:03:06 +0200

    ajout du fichier config.js et d'un préfixe

--- index.js
+++ index.js
@@ -1,14 +1,20 @@
 const Discord = require('discord.js');
+const { TOKEN, PREFIX } = require('./config');
 const client = new Discord.Client();
 
 client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}!`);
+  console.log("c'est bon !");
 });
 
 client.on('message', msg => {
-  if (msg.content === '-ping') {
-    msg.reply('pong');
+  if (msg.content.startsWith(`${PREFIX}test`)) {
+    msg.channel.send({
+      embed: {
+        color: 9782005,
+        description: ""
+      }
+    });
   }
 });
 
-client.login('NjE1NzAzNDY0NjMwMDI2MjQw.XWR87A.yeZ2haruBiKVLaj2sLHAg2f-N54');
+client.login(TOKEN);
